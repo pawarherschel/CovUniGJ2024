4fd81b79aaf082caaf5badb464e8c14c1eb0cddd
pawarherschel <pawarherschel@gmail.com>
2024-07-23 17:59:06 +0530

    updated player controller

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Script
 {
-    // Start is called before the first frame update
-    void Start()
+    public sealed class PlayerController : MonoBehaviour
     {
+        private Rigidbody2D _rigidbody2D;
         
-    }
+        [SerializeField] private float maxHSpeed = 3f;
+        [SerializeField] private float horizontalSpeed = 10f;
+        private bool _canJump = true;
+        [SerializeField] private float jumpForce = 7.5f;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private PrefabSwitcherOnTimer _switcherOnTimer;
         
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rigidbody2D = this.GetComponent<Rigidbody2D>();
+            _switcherOnTimer = this.GetComponent<PrefabSwitcherOnTimer>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            _rigidbody2D.AddForce(Vector2.right * (Input.GetAxis("Horizontal") * horizontalSpeed));
+            if (Input.GetKeyDown(KeyCode.Space) && _canJump)
+            {
+                var animator = _switcherOnTimer.CurrentSprite.GetComponent<Animator>();
+                // animator.Play("jump");
+                _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                _canJump = false;
+            }
+            
+            if (_rigidbody2D.velocity.x < -maxHSpeed || _rigidbody2D.velocity.x > maxHSpeed)
+            {
+                var sign = _rigidbody2D.velocity.x >= 0 ? 1 : -1;
+                _rigidbody2D.velocity = new Vector2(sign * maxHSpeed, _rigidbody2D.velocity.y);
+            }
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            _canJump = true;
+        }
     }
 }
