d41ccd82919b054a2b212829099474a98aeb78ed
pawarherschel <pawarherschel@gmail.com>
2024-07-26 06:12:34 +0530

    boss kinda working now

--- Assets/Script/BossController.cs
+++ Assets/Script/BossController.cs
@@ -10,6 +10,7 @@ namespace Script
         
         private GameObject _player;
         private BossPrefabSwitcherOnTimer _switcherOnTimer;
+        private Rigidbody2D _rigidbody2D;
 
         // Start is called before the first frame update
         private void Start()
@@ -26,6 +27,9 @@ namespace Script
 
             var internalBossController = _switcherOnTimer.CurrentInternalController;
             Debug.Assert(internalBossController != null, nameof(internalBossController) + " != null");
+
+            _rigidbody2D = this.GetComponent<Rigidbody2D>();
+            Debug.Assert(_rigidbody2D != null, nameof(_rigidbody2D) + " != null");
         }
 
         // Update is called once per frame
@@ -34,36 +38,44 @@ namespace Script
             var canAttack = CanAttack();
             var canChase = CanChase();
 
+            bool? attackSuccess = null;
+            bool? chaseSuccess = null;
 
             if (canAttack)
             {
-                var attackSuccess = _switcherOnTimer.CurrentInternalController.Attack();
+                _rigidbody2D.velocity = Vector2.zero;
+                _switcherOnTimer.CurrentInternalController.StopChase();
+                attackSuccess = _switcherOnTimer.CurrentInternalController.Attack();
             }
-            if (canChase)
+            if (canChase && !canAttack)
             {
-                var chaseSuccess = _switcherOnTimer.CurrentInternalController.Chase();
+                chaseSuccess = _switcherOnTimer.CurrentInternalController.Chase();
+            }
+            else
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+                _switcherOnTimer.CurrentInternalController.StopChase();
             }
 
-            if (canChase && canAttack)
-            {
-                _switcherOnTimer.CurrentInternalController.StopChase();
-                _switcherOnTimer.CurrentInternalController.Attack();
-            }
+            if (!(chaseSuccess ?? false)) return;
+            var direction = ((_player.transform.position.x - this.transform.position.x) < 0 ? -1 : 1);
+            _rigidbody2D.velocity = Vector2.right * (hSpeed * direction);
         }
 
         private bool CanChase()
         {
-            var chasePositionX = chasePosition.position.x;
-            var playerInRange = Mathf.Abs(_player.transform.position.x - chasePositionX) <
-                                chasePositionX;
+            var position = chasePosition.position;
+            var playerInRange = _player.transform != null && Vector2.Distance(_player.transform.position, position) <
+                Vector2.Distance(Vector2.zero, position);
 
             return playerInRange;
         }
 
         private bool CanAttack()
         {
-            var attackPositionX = attackPosition.position.x;
-            var playerInRange = Mathf.Abs(_player.transform.position.x - attackPositionX) < attackPositionX;
+            var position = attackPosition.position;
+            var playerInRange = _player.transform != null && Vector2.Distance(_player.transform.position, position) <
+                Vector2.Distance(Vector2.zero, position);
 
             return playerInRange;
         }
